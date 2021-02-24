287fd6960c4ef8437c45bce6e7db1ebe9b05e8ad
Tetiana Panasiuke <tetiana.panasiuk21@gmail.com>
2021-02-24 21:37:39 +0200

    Repeated basic js

---
+++ JavaScript_Practice/basic_js/script.js
@@ -0,0 +1,65 @@
+// const firstName = "Tania";
+// const age = 27;
+// const $ = "some string";
+// alert("Persone name: " + name + ", person age: " + age.toString());
+// console.log("Persone name: " + name + ", person age: " + age.toString());
+
+// const lastName = prompt("Enter your last name");
+// alert(firstName + " " + lastName);
+
+// const currentYear = 2020;
+// const birthYear = 1993;
+// const age = currentYear - birthYear;
+// console.log(age);
+
+// Типи даних
+// const isProgrammer = true;
+// const name = "Tania";
+// const age = 27;
+// let x;
+// console.log(typeof isProgrammer);
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof x);
+
+// Пріоритет операторів
+// const fullAge = 27;
+// const birthYear = 1993;
+// const currentYear = 2021;
+// const isFullAge = currentYear - birthYear >= fullAge;
+// console.log(isFullAge);
+
+// Умовні оператори
+// const coureStatus = "not ready";
+// if (coureStatus === "ready") {
+//   console.log("Course is ready");
+// } else if (coureStatus === "pending") {
+//   console.log("Course is on process");
+// } else {
+//   console.log("Course is not ready yet");
+// }
+
+// const num1 = 42;
+// const num2 = "42";
+// console.log(num1 === num2);
+
+// const isReady = true;
+// if (isReady) {
+//   console.log("All is ready");
+// } else {
+//   console.log("All is not ready yet");
+// }
+
+// isReady ? console.log("All is ready") : console.log("All is not ready yet");
+
+// function calculateAge(year) {
+//   return 2020 - year;
+// }
+// const myAge = calculateAge(1993);
+// console.log(myAge);
+
+// function logInfoAbout(name, year) {
+//   const age = calculateAge(year);
+//   console.log("Person name: " + name + " and her age is" + " " + age);
+// }
+// logInfoAbout("Tania", 1993);
