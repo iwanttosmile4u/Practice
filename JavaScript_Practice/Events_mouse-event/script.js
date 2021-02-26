886863947ca9bd0592f79b55f7bf5d6ad8540b43
Tetiana Panasiuke <tetiana.panasiuk21@gmail.com>
2021-02-26 11:26:32 +0200

    Some changes

--- JavaScript_Practice/Events_mouse-event/script.js
+++ JavaScript_Practice/Events_mouse-event/script.js
@@ -2,25 +2,29 @@
  * Show alert with message 'This is click', after the click on the 'Click me' button.
  */
 
+// const clickMe = document.getElementById("clickMeBtn");
+// clickMe.addEventListener("click", () => {
+//   alert("This is click");
+// });
+
 /*TASK - 2
  * Show alert with message 'This is mouseover', after the hover on the 'Click me' button.
  */
 
 // const clickMe = document.getElementById("clickMeBtn");
-// clickMe.onmouseover = () => {
+// clickMe.addEventListener("mouseover", () => {
 //   alert("This is mouseover");
-// };
+// });
 
 /*TASK - 3
  * Create a function, that will be changing the background color of the 100px square randomly,
  * by the clicking on it. Every color should be random, transparent and white are not included in the list of colors.
  */
 
-/*
- * 1 - научиться генерировать случайное число от 0 до n
- * 2 - при помощи JS создать квадрат, разместить его на странице
- * 3 - добавить слушатель клика на квадрат, вывести в консоль "square clicked"
- * 4 - менять цвет фона квадрату по клику
- * */
-
-function getRandomColor() {}
+function getRandomColor() {
+  let firstColor = Math.floor(Math.random() * 255);
+  let secondColor = Math.floor(Math.random() * 255);
+  let thirdColor = Math.floor(Math.random() * 255);
+  return `rgb(${firstColor}), ${secondColor}, ${thirdColor}`;
+}
+console.log(getRandomColor());
