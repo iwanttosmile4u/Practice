87d0ed5acc3431308d9b2c50b1251fa54088b6fb
Tetiana Panasiuke <tetiana.panasiuk21@gmail.com>
2021-04-24 11:48:00 +0300

    practice with fetch

---
+++ JavaScript_Practice/ajax_fetch/src/js/task1.js
@@ -0,0 +1,22 @@
+/* ЗАДАЧА-01
+ * Написать json файл, который будет сожержать любое количество обнотипных объектов пользователей.
+ * При помощи XMLHttpRequest получить эти данные из файла.
+ * */
+
+// const url = "user.json";
+// const request = new XMLHttpRequest();
+// request.open("GET", url);
+// request.onload = function (e) {
+//   if (e.target.status === 200) {
+//     const result = JSON.parse(e.target.response);
+//     console.log(result);
+//   }
+// };
+// request.send();
+
+// fetch(url)
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((e) => console.error(e));
