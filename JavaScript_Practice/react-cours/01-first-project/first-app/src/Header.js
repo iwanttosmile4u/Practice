f489f6a68b05282918e6b570cccfd6140a5b8771
Tetiana Panasiuke <tetiana.panasiuk21@gmail.com>
2021-05-21 17:44:08 +0300

    practice with react

---
+++ JavaScript_Practice/react-cours/01-first-project/first-app/src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <a href="#">Home</a> - <a href="#">News Feed</a> -
+        <a href="#">Messages</a> -
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ JavaScript_Practice/react-cours/01-first-project/first-app/src/Technologies.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Technologies extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>css</li>
+          <li>html</li>
+          <li>js</li>
+          <li>react</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Technologies;
