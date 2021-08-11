789ab187cdca36cc60ded8b73ef8bad3caaa715a
Tetiana Panasiuke <tetiana.panasiuk21@gmail.com>
2021-08-11 09:50:39 +0300

    solved new tasks

--- JavaScript_Practice/basic_js/03-script.js
+++ JavaScript_Practice/basic_js/03-script.js
@@ -2,5 +2,19 @@
 // // alert(`Hello ${name + 1}`);
 // alert(`Hello ${"name"}`);
 
-const billion = 1e9;
-console.log(billion);
+// const billion = 1e9;
+// console.log(billion);
+
+// let num = 9.7;
+// console.log(Math.floor(num));
+
+// let num = 34.657;
+// let newNum = Number(num.toFixed(2));
+// console.log(newNum);
+
+// let value = 234.5;
+// console.log(isNaN(value));
+
+// let firstNum = +prompt("Enter the first number");
+// let secondNum = +prompt("Enter the second number");
+// console.log(firstNum + secondNum);
