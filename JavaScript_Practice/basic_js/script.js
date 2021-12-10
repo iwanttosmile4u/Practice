bcf92a731cd42baac7a3b2be200b08804977946d
Tetiana Panasiuke <tetiana.panasiuk21@gmail.com>
2021-12-10 13:39:26 +0200

    some practice with operators

--- JavaScript_Practice/basic_js/script.js
+++ JavaScript_Practice/basic_js/script.js
@@ -63,3 +63,95 @@
 //   console.log("Person name: " + name + " and her age is" + " " + age);
 // }
 // logInfoAbout("Tania", 1993);
+
+// let _c = document.body;
+// console.log(_c);
+
+// let $ = true;
+// console.log($);
+
+// let a = BigInt(123);
+// console.log(typeof a);
+
+// let myVar;
+// console.log(typeof myVar);
+
+// let a = 5;
+// console.log(a++);
+// console.log(a);
+
+// var b = 10;
+// if (b % 3 == 0) {
+//   var i = 10;
+// }
+// console.log(i);
+
+// var a = 5;
+// var test = 5 == a ? "Yes" : "No";
+// console.log(test);
+
+// var a = false;
+// if (a == `0`) {
+//   alert(`==`);
+// }
+// if (a === `0`) {
+//   alert(`===`);
+// }
+
+// var bool = new Boolean(false);
+// if (bool) {
+//   console.log(`Hello`);
+// } else {
+//   console.log(`Error`);
+// }
+
+// var a = 3;
+// switch (a) {
+//   default:
+//     a += 4;
+//   case 1:
+//     a += 2;
+//     break;
+//   case 2:
+//     a += 3;
+//     break;
+// }
+// console.log(a);
+
+// var x = 3;
+// {
+//   var x = 5;
+// }
+// console.log(x);
+
+// var a = 5;
+// var test = 5 == a ? "Yes" : "No";
+// console.log(a);
+
+// for (var i = 0; i < 10; ) {
+//   i++;
+// }
+// console.log(i);
+
+// for (let i = 0; i < 3; i++) {
+//   console.log(i);
+// }
+
+// for (var i = 10; i < 35; i += 5) {
+//   console.log(i);
+// }
+
+// var i = 0;
+// while (i++ < 5) {
+//   alert(i);
+// }
+
+// var i = 3;
+// while (i) {
+//   alert(i--);
+// }
+
+for (var i = 0; i > -10; i -= 3) {
+  i++;
+  console.log(i);
+}
