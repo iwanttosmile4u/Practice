29b0ba88ae5d35642be55caa6456e1dfcb601809
Tetiana Panasiuke <tetiana.panasiuk21@gmail.com>
2022-01-07 20:15:52 +0200

    complited sorting

--- vue/app-practice/src/App.vue
+++ vue/app-practice/src/App.vue
@@ -1,34 +1,90 @@
 <template>
   <div class="app">
-    <post-form @create="createPost" />
-    <post-list :posts="posts" @remove="removePost" />
+    <h1>Page with posts</h1>
+    <!-- <input type="text" v-model.number="modificatorValue" /> -->
+    <div class="app-btns">
+      <my-button @click="showDialog">Create post</my-button>
+      <my-select v-model="selectedSort" :options="sortOptions" />
+    </div>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form @create="createPost" />
+    </my-dialog>
+    <post-list
+      :posts="sortedPosts"
+      @remove="removePost"
+      v-if="!isPostsLoading"
+    />
+    <div v-else>Loading...</div>
   </div>
 </template>
 <script>
 import PostForm from './components/PostForm.vue';
 import PostList from './components/PostList.vue';
+import MyButton from './components/UI/MyButton.vue';
+import axios from 'axios';
+import MySelect from './components/UI/MySelect.vue';
 export default {
   components: {
     PostList,
-    PostForm
+    PostForm,
+    MyButton,
+    MySelect
   },
   data() {
     return {
-      posts: [
-        { id: 1, title: 'JavaScript', body: 'Post description' },
-        { id: 1, title: 'JavaScript 2', body: 'Post description 2' },
-        { id: 1, title: 'JavaScript 3', body: 'Post description 3' }
+      posts: [],
+      dialogVisible: false,
+      // modificatorValue: ''
+      isPostsLoading: false,
+      selectedSort: '',
+      sortOptions: [
+        { value: 'title', name: 'Sort by title' },
+        { value: 'body', name: 'Sort by body' }
       ]
     };
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.dialogVisible = false;
     },
     removePost(post) {
       this.posts = this.posts.filter((p) => p.id !== post.id);
+    },
+    showDialog() {
+      this.dialogVisible = true;
+    },
+    async fetchPosts() {
+      try {
+        this.isPostsLoading = true;
+        const response = await axios.get(
+          'https://jsonplaceholder.typicode.com/posts?_limit=10'
+        );
+        this.posts = response.data;
+      } catch (e) {
+        alert('Error!');
+      } finally {
+        this.isPostsLoading = false;
+      }
+    }
+  },
+  mounted() {
+    this.fetchPosts();
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) =>
+        post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+      );
     }
   }
+  // watch: {
+  //   selectedSort(newValue) {
+  //     this.posts.sort((post1, post2) => {
+  //       return post1[newValue]?.localeCompare(post2[newValue]);
+  //     });
+  //   }
+  // }
 };
 </script>
 
@@ -41,5 +97,11 @@ export default {
 
 .app {
   padding: 20px;
+}
+
+.app-btns {
+  display: flex;
+  justify-content: space-between;
+  margin: 15px 0;
 }
 </style>
